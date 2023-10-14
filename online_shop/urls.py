d528fcf94fe434eae9d00922db8b9c7b42897734
Бактияр <67044578+b7-coder@users.noreply.github.com>
2023-10-14 18:11:09 +0600

    first commit

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from shop_site.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('contact/', contact, name='contact'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
--- shop_site/views.py
+++ shop_site/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def contact(request):
+    if request.method == "POST":
+        firstname =  request.POST.get('firstname')
+        lastname =  request.POST.get('lastname')
+        nomer =  request.POST.get('nomer')
+        mail =  request.POST.get('mail')
+        message =  request.POST.get('message')
+
+        # Cохранить заявку от пользователя
+    
+    return render(request, "contact.html")
